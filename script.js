164d7d54d229af4fd1a3097fed95fb0a6da15f6c
jellyfish-383 <echiba834@gmail.com>
2025-05-29 12:06:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,16 @@
           try {
             const device = await navigator.bluetooth.requestDevice({
               acceptAllDevices: true,
+                filters: [
+                        { name: 'web bluetooth api' }  // デバイス名でフィルタリング
+                ],      
               optionalServices: ['battery_service'] // 仮でバッテリーサービス
+                    .then(device => {
+  console.log('選択されたデバイス:', device.name);
+})
+.catch(error => {
+  console.error('エラー:', error);
+});
             });
     
             const server = await device.gatt.connect();
@@ -19,39 +28,3 @@
         });
 
 
-navigator.bluetooth.requestDevice({
-  filters: [
-    { name: 'web bluetooth api' }  // デバイス名でフィルタリング
-  ],
-  optionalServices: ['battery_service']
-})
-.then(device => {
-  console.log('選択されたデバイス:', device.name);
-})
-.catch(error => {
-  console.error('エラー:', error);
-});
-
-navigator.bluetooth.requestDevice({
-  filters: [{ services: ['00001101-0000-1000-8000-00805F9B34FB'] }]  // SPP (Serial Port Profile)
-})
-.then(device => {
-  return device.gatt.connect();
-})
-.then(server => {
-  return server.getPrimaryService('00001101-0000-1000-8000-00805F9B34FB');  // SPPサービス
-})
-.then(service => {
-  // シリアル通信でデータを読み書き
-  return service.getCharacteristic('00001101-0000-1000-8000-00805F9B34FB');
-})
-.then(characteristic => {
-  // データの読み書き（例えば、文字列を送信する）
-  return characteristic.writeValue(new TextEncoder().encode('Hello, HM-10!'));
-})
-.then(() => {
-  console.log('データが送信されました');
-})
-.catch(error => {
-  console.log('エラー:', error);
-});
